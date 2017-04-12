086816bfe818464563b991e8e2022c1108ba6adb
Dr. Orion Lawlor <lawlor@alaska.edu>
2017-04-12 14:50:39 -0800

    Add obstacle avoidance, including distance to the robot parts.  Should even work for an L shaped robot, although it might need a minus sign somewhere.

--- path_planning/gridnav/gridnav.h
+++ path_planning/gridnav/gridnav.h
@@ -14,12 +14,30 @@ Aurora Robotics 2017
 #define AURORA_GRIDNAV_H
 
 #include <iostream>
-#include <deque>
-#include <queue>
+#include <deque> 
+#include <queue> // for priority_queue
+#include <functional> // for reference_wrapper
 #include <math.h>
 #include "osl/vec2.h"
 
 namespace gridnav {
+
+  // Clearance height for open parts of robot
+  enum {OPEN=1000};
+  
+  // User-defined class for robot geometric clearances
+  class robot_geometry {
+  public:
+    virtual ~robot_geometry() {}
+    
+    // Return the clearance height at this (x,y) robot position in centimeters.
+    //    (0,0) is the robot's origin, at the center of turning.
+    //    +x is the direction the robot naturally drives forward
+    //    Height 0 collides with all obstacles (e.g., tracks).
+    //    Height 10 collides with obstacles of height >=10.
+    //    Height OPEN does not collide with anything.
+    virtual int clearance_height(float x,float y) const =0;
+  };
 
 /**
  Create a navigation framework for a space with:
@@ -49,6 +67,8 @@ public:
       if (a<0 || a>=GRIDA) return false;
       return true;
     }
+    
+    gridposition(int x_,int y_,int a_) :x(x_), y(y_), a(a_) {}
     
     gridposition(const fposition &p) {
       x=(int)floor(p.v.x*(1.0/GRIDSIZE)+0.5);
@@ -113,6 +133,15 @@ public:
         data[y][x]=clearvalue;
     }
     
+    void print(void) {
+      for (int y=0;y<GRIDY;y++) {
+        for (int x=0;x<GRIDX;x++) {
+          std::cout<<data[y][x];
+        }
+        std::cout<<"\n";
+      }
+    }
+    
     // Accessors, without bounds check
     T &at(int x,int y) { return data[y][x]; }
     const T &at(int x,int y) const { return data[y][x]; }
@@ -121,12 +150,6 @@ public:
   // This represents everything we know about one slice of our domain
   class gridslice {
   public:
-    // This is our robot orientation angle in degrees
-    float ang_deg;
-
-    // This is our robot orientation angle in radians
-    float ang_rad;
-
     // In this slice, the robot can drive in this direction
     //   This is a unit vector = vec2(cos(ang_rad),sin(ang_rad))
     vec2 drive;
@@ -141,27 +164,122 @@ public:
   // The whole grid is just a list of slices, indexed by angle.
   gridslice slice[GRIDA];
   
+  // Return the drive direction at this grid angle ID, in radians
+  static float angle_to_radians(float ia) { return ia*(2.0*M_PI/GRIDA); }
+  // Return the drive direction at this grid angle ID, in degrees
+  static float angle_to_degrees(float ia) { return ia*(360.0/GRIDA); }
+  
   // Set up our slices, assuming no obstacles and a point robot
   gridnavigator() {
     for (int ia=0;ia<GRIDA;ia++) {
       gridslice &s=slice[ia];
-      float ang=ia*(2.0*M_PI/GRIDA);
-      s.ang_deg=ia*(360.0/GRIDA);
-      s.ang_rad=ang;
+      float ang=angle_to_radians(ia);
       s.drive=vec2(cos(ang),sin(ang));
       s.obstacle.clear(0);
       s.visit.clear(0);
     }
+    obstacles.clear(' ');
+    lastpath.clear(' ');
+  }
+
+/************ Robot Geometry ************************/
+  // At a given angle, this lists the discretized grid points relative to the robot origin.
+  //   x,y are relative to the robot origin.  a=clearance_height.
+  typedef std::vector<gridposition> robot_grid_slice;
+  
+  // Discretized grid version of robot geometry.
+  //   Build this once, at startup.
+  class robot_grid_geometry {
+  public:
+    // For each angle, this gives the corresponding robot clearances
+    robot_grid_slice slice[GRIDA];
+    
+    robot_grid_geometry(const robot_geometry &geo,bool verbose=false) {
+      // For each rotation angle:
+      for (int ia=0;ia<GRIDA;ia++) {
+        if (verbose) std::cout<<"Robot geometry at grid angle "<<angle_to_degrees(ia)<<"\n";
+        robot_grid_slice &robotslice=slice[ia];
+        float ang=angle_to_radians(ia);
+        float c=cos(ang), s=sin(ang);
+        
+        enum {CHECK=2*ROBOTGRID}; // grid cells to search
+
+        // Loop over grid cells relative to robot origin:
+        // for (int y=-CHECK;y<=+CHECK;y++)
+        for (int y=+CHECK;y>=-CHECK;y--) // <- count down, so ascii art is upright
+        for (int x=-CHECK;x<=+CHECK;x++) {
+
+            int height=OPEN;
+            
+            // Check robot clearance height all through this grid cell:
+            for (double dy=-0.6;dy<=+0.6;dy+=0.125)
+            for (double dx=-0.6;dx<=+0.6;dx+=0.125) {
+              vec2 g(GRIDSIZE*(x+dx),GRIDSIZE*(y+dy)); // grid position
+              // rotate to robot coordinates
+              vec2 r( c*g.x + s*g.y,
+                     -s*g.x + c*g.y );
+              height=std::min(height,geo.clearance_height(r.x,r.y));
+            }
+            
+            char height_debug=' ';
+            if (height<OPEN) {
+              robotslice.push_back(gridposition(x,y,height));
+              height_debug='0'+height;
+            }
+            if (verbose && y%2==0) { // skip odd rows to fix aspect ratio
+              if (y==0 && x==0) height_debug='+'; // origin
+              if (y==2 && x==0) height_debug='y'; // +y direction
+              if (y==0 && x==1) height_debug='x'; // +x direction
+              std::cout<<height_debug; // ascii art picture
+              if (x==+CHECK) std::cout<<"\n";
+            }
+        }
+      }
+    }
+  };
+  
+  
+  // This grid stores all known obstacles (for debug, navigation uses the sliced versions)
+  grid2D<char> obstacles;
+  
+  // This grid stores the winning path
+  grid2D<char> lastpath;
+  
+  // Mark this obstacle location, xy in grid coordinates, as impassible for this robot.
+  //  This can be called at startup, or at runtime for new obstacles.
+  void mark_obstacle(int x,int y,int height, const robot_grid_geometry &robot) {
+    if (gridposition(x,y,0).valid()) obstacles.at(x,y)='0'+height;
+    for (int ia=0;ia<GRIDA;ia++) {
+      gridslice &navslice=slice[ia];
+      const robot_grid_slice &robotslice=robot.slice[ia];
+      for (gridposition g : robotslice) {
+        if (g.a<height) { // robot would hit this obstacle--mark center as impassible
+          gridposition hit(x-g.x, y-g.y, ia);
+          if (hit.valid())
+            navslice.obstacle.at(hit.x,hit.y)=height;
+        }
+      }
+    }
   }
   
-  // FIXME: mark obstacles based on robot geometry (somehow)
+  // Mark the edges of the grid as impassible for this robot
+  void mark_edges(const robot_grid_geometry &robot) {
+    int boundary=10;
+    for (int y=-boundary;y<=GRIDY+boundary;y++)
+    for (int x=-boundary;x<=GRIDX+boundary;x++)
+    {
+      if (!gridposition(x,y,0).valid())
+        mark_obstacle(x,y,99,robot);
+    }
+  }
   
 
+/*************** Path Planning *************************/
   // Convert discrete angles to grid cell equivalent.
   //   This scale factor is derived figuring the tip velocity of a turning robot.
   static constexpr double TURN_COST_TO_GRID_COST=((2.0*M_PI/GRIDA)*(ROBOTGRID*0.5));
 
-  // This class represents an intermediate position in a search.
+  // This class represents an intermediate position in a path search.
   class searchposition {
   public:
     double cost; // cost to get here
@@ -173,24 +291,22 @@ public:
       :cost(cost_), p(p_), target(target_), last(last_)
     {}
     
+    float angle_dist(float delta) const {
+       delta=fmodplus(delta,GRIDA);
+       if (delta>GRIDA/2) delta=GRIDA-delta; // turn the other way
+       return delta;
+    }
+    
     // Return the approximate cost to reach the target
     double total_cost(void) const {
       double drive_dist=length(p.v-target.v); // in grid cells
-      double turn_ang=fabs(p.a-target.a); // in discrete angle units
+      double turn_ang=angle_dist(p.a-target.a); // in discrete angle units
       return cost + drive_dist + turn_ang*TURN_COST_TO_GRID_COST;
     }
-  };
-  
-  // Keeps a reference to a searchposition.
-  //   Needed because std::priority_queue<searchposition &> fails for reasons that are not clear.
-  class searchposition_ref {
-  public:
-    const searchposition *r;
-    searchposition_ref(const searchposition &r_) :r(&r_) {}
     
     // Sort so we always grab the best search position first
-    bool operator<(const searchposition_ref &p) const {
-      return r->total_cost()>p.r->total_cost();
+    bool operator>(const searchposition &p) const {
+      return total_cost() > p.total_cost();
     }
   };
   
@@ -205,11 +321,15 @@ public:
     bool verbose;
     
     // This is the active list of searched positions, sorted by distance to target.
-    std::priority_queue<searchposition_ref> search;
+    std::priority_queue<std::reference_wrapper<searchposition>,
+      std::vector< std::reference_wrapper<searchposition> >,
+      std::greater<searchposition> > search;
     
     // SUBTLE: to allow backtracking, we need to persistently store search positions.
     //  std::deque has the nice property of not invalidating references.
     std::deque<searchposition> pool; // safely stores our search positions
+    
+    // Add this search position to our priority queue
     void add_search(double cost,const fposition &origin,const searchposition *last) {
       pool.emplace_back(cost,origin,target,last);
       search.push(pool.back());
@@ -218,6 +338,7 @@ public:
   public:
     // This bool marks if we found a path
     bool valid;
+    
     // This is the sequence of steps from origin to target
     std::deque<fposition> path;
     
@@ -228,13 +349,14 @@ public:
       for (int ia=0;ia<GRIDA;ia++) {
         nav.slice[ia].visit.clear(0);
       }
+      nav.lastpath.clear(' ');
       
       // Start search at origin
       add_search(0.0,origin_,0);
     
       // Repeatedly visit positions with the cheapest net cost
       while (!search.empty()) {
-        const searchposition &cur=*(search.top().r); search.pop();
+        const searchposition &cur=search.top(); search.pop();
         
         if (verbose) std::cout<<"At "<<cur.p<<" cost "<<cur.cost<<": ";
         
@@ -261,12 +383,16 @@ public:
         if (verbose) std::cout<<"pushing children\n";
         
         // Check if we're done:
-        if (gridposition(cur.p)==gridposition(target)) 
+        gridposition gcur(cur.p);
+        if (gcur.valid()) nav.lastpath.at(gcur.x,gcur.y)='.';
+        if (gcur==gridposition(target)) 
         { // we're done!  Follow the chain of "last" pointers back to the origin.
           if (verbose) std::cout<<"Target reached!  Reverse path:\n";
           for (const searchposition *p=&cur;p!=NULL;p=p->last) {
             path.push_front(p->p);
             std::cout<<p->p<<"\n";
+            gridposition gpath(p->p);
+            if (gpath.valid()) nav.lastpath.at(gpath.x,gpath.y)='x';
           }
           valid=true;
           return;
--- path_planning/gridnav/main.cpp
+++ path_planning/gridnav/main.cpp
@@ -3,18 +3,51 @@
 */
 #include "gridnav.h"
 
+class rmc_robot_geometry : public gridnav::robot_geometry {
+public:
+    //    (0,0) is the robot's origin, at the center of turning.
+    //    +x is the direction the robot naturally drives forward
+  virtual int clearance_height(float x,float y) const 
+  {
+  // front-back:
+    if (x>40.0 || x<-35.0) return gridnav::OPEN;
+    if (y<0) y=-y; // apply Y symmetry
+    if (y>75.0) return gridnav::OPEN; // beyond the tracks
+    if (y>45.0) return 0; // tracks
+    return 20; // open area under mining head
+  }
+};
+
 int main() 
 {
+  // RMC-plausible navigation grid dimensions:
   typedef gridnav::gridnavigator<38,74, 64, 10, 15> navigator_t;
   navigator_t navigator;
+  
+  // Discretize the robot geometry
+  rmc_robot_geometry geo;
+  navigator_t::robot_grid_geometry robot(geo,true);
+  
+  // Mark impassable edges
+  navigator.mark_edges(robot);
+  // Make some obstacles on left side
+  for (int x=0;x<10;x++)
+  navigator.mark_obstacle(x,30,40,robot);
+  // Make a straddleable obstacle in middle
+  for (int x=20;x<23;x++)
+  navigator.mark_obstacle(x,30,30,robot);
+  
+  // Plan a path
   typedef navigator_t::fposition fposition;
   
-  fposition start(200,500,0);
-  fposition target(190,50,90);
+  fposition start(70,600,20);
+  fposition target(190,40,90);
   
   navigator_t::planner plan(navigator,start,target,true);
   for (const fposition &p : plan.path)
     std::cout<<"Plan position: "<<p<<"\n";
+  
+  navigator.lastpath.print();
   
   return 0;
 }
